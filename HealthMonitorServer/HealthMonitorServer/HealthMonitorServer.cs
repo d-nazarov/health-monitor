cbe4a1c9abae1aa23bbf98909a896c4b6b046855
d.nazarov <nazarov.dmirty.a@gmail.com>
2016-10-25 00:16:46 +0300

    WebSocket server added

---
+++ HealthMonitorServer/HealthMonitorServer/HealthMonitorServer.cs
@@ -0,0 +1,36 @@
+﻿using Fleck;
+using System.Collections.Generic;
+
+namespace HealthMonitorServer
+{
+    public class HealthMonitorServer
+    {
+        private List<IWebSocketConnection> connections = new List<IWebSocketConnection>();
+        private object sync = new object();
+
+        private readonly WebSocketServer server;
+
+        public HealthMonitorServer(string host, string port)
+        {
+            server = new WebSocketServer(string.Format("ws://{0}:{1}", host, port));
+
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    lock (sync)
+                    {
+                        connections.Add(socket);
+                    }
+                };
+                socket.OnClose = () =>
+                {
+                    lock (sync)
+                    {
+                        connections.Remove(socket);
+                    }
+                };
+            });
+        }
+    }
+}
--- HealthMonitorServer/HealthMonitorServer/Program.cs
+++ HealthMonitorServer/HealthMonitorServer/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 
 namespace HealthMonitorServer
 {
@@ -10,6 +7,9 @@ namespace HealthMonitorServer
     {
         static void Main(string[] args)
         {
+            var server = new HealthMonitorServer(ConfigurationSettings.AppSettings["Host"], ConfigurationSettings.AppSettings["Port"]);
+
+            Console.ReadLine();
         }
     }
 }
